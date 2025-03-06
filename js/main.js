15d8b068a33862013cbdbceb981527e67f095ffe
Ivan Mamichau <imamichau@Ivans-MacBook-Pro.local>
2025-03-06 01:57:16 +0100

    update secret phrase

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Function to check user input
 function checkText() {
-    const correctText = "bloom surprise joy"; // The secret phrase
+    const correctText = "bloom joy feel love surprise"; // The secret phrase (цвети, радуйся, чувствуй любовь, удивляйся)
     const userInput = document.getElementById("userInput").value;
 
     // If the user input matches the correct text, redirect to video page
-    if (userInput === correctText.trim().toLowerCase()) {
+    if (userInput.trim().toLowerCase() === correctText) {
         window.location.href = "video.html"; // Redirect to the video page
     } else {
         alert("Incorrect words. Please try again. Try to find correct words on the postcard."); // Show alert for incorrect input
